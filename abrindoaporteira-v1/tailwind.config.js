343ef9a8bb877a6b8e006600e77b759c1b487c52
AES MARKETING LTDA <infra@aesmkt.com.br>
2025-01-16 19:19:28 +0000

    remove preload de fonte e simplifica a configuração da família de fontes

--- abrindoaporteira-v1/tailwind.config.js
+++ abrindoaporteira-v1/tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
 
       },
       fontFamily: {
-        'roboto': ['Roboto',  'system-ui', 'sans-serif', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Open Sans', 'Helvetica Neue', 'Arial',],
+        'roboto': ['Roboto',  'system-ui', 'sans-serif', '-apple-system', 'Arial',],
     },
   },
   },
